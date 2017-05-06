f2808d57b5bc73746de0bd19f6e8797398560d9e
godfern <godfern13@gmail.com>
2017-05-07 00:20:41 +0530

    Added jquery, bootstrap

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ var express = require('express');
 var app = express();
 
 //app.use(express.static('public'));
-// app.use('/scripts', express.static(__dirname + '/node_modules/angular/'));
+ app.use('/node_modules', express.static(__dirname + '/node_modules/'));
+
+ app.use('/css', express.static(__dirname + '/public/css/'));
+ app.use('/js', express.static(__dirname + '/public/js/'));
 
 app.get('*', function (req, res) {
     //res.send('Hello World!');
-    res.sendfile('./public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(process.env.PORT || 3000, function () {
---
+++ public/js/script.js
@@ -0,0 +1,36 @@
+(function(){
+
+	// var homeBtn = document.getElementById('home');
+	// var bioBtn = document.getElementById('bio');
+	// var workBtn = document.getElementById('work');
+	var linkBtn = document.getElementsByClassName('nav-link');
+	var defaultBlock = document.getElementById('home');
+	var prevClass = defaultBlock;
+
+
+
+	// linkBtn[0].addEventListener('click', function(e) {  do stuff here 
+	// 	console.log(e.target);
+	// }, false);
+	// bioBtn.addEventListener('click', function() { /* do stuff here*/ }, false);
+	// workBtn.addEventListener('click', function() { /* do stuff here*/ }, false);
+	// contactBtn.addEventListener('click', function() { /* do stuff here*/ }, false);
+	// 
+	var myFunction = function(e) {
+	    var attribute = this.getAttribute("data-button");
+
+	    if(prevClass){
+	    	// prevClass.remove('show');
+	    	prevClass.classList.remove("show");
+	    }
+	    var containerBlc = document.getElementById(attribute);
+	    prevClass = containerBlc;
+	    containerBlc.className = 'show';
+
+	};
+
+	for (var i = 0; i < linkBtn.length; i++) {
+	    linkBtn[i].addEventListener('click', myFunction, false);
+	}
+
+})();
